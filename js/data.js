3452dfba387b1f5560e7005fdc26c897be552641
QUANG-PTIT-HN-065 <quangchsga@example>
2025-04-22 18:06:28 +0700

    feat/userPage

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
 let Account = {};
+const listQuestion = JSON.parse(localStorage.getItem("listQuestion")) || [
+    {
+        id: 1,
+        question: "Câu hỏi 1",
+        answer: "Đáp án 1",
+    },
+    {
+        id: 2,
+        question: "Câu hỏi 2",
+        answer: "Đáp án 2",
+    },
+    {
+        id: 3,
+        question: "Câu hỏi 3",
+        answer: "Đáp án 3",
+    },
+];
